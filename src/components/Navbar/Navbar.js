63f7908fdfc60e8e4b68ce66d8a15afbffc78657
kelseysanderson <78172284+kelseysanderson@users.noreply.github.com>
2021-05-13 00:20:50 -0500

    updated footer and navbar styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,54 +3,54 @@ import { Link } from "react-router-dom";
 import "./navbar.css";
 
 function Navbar() {
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <h1>KELSEY SANDERSON</h1>
-          <div>
-            <ul className="navbar-nav">
-            <li className="nav-item">
-                <Link
-                  to="/"
-                  className={
-                    window.location.pathname === "/"
-                      ? "nav-link active"
-                      : "nav-link"
-                  }
-                >
-                  Home
-                </Link>
-              </li>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <p>KELSEY SANDERSON</p>
+      <div>
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <Link
+              to="/"
+              className={
+                window.location.pathname === "/"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Home
+              </Link>
+          </li>
 
-              <li className="nav-item">
-                <Link
-                  to="/about"
-                  className={
-                    window.location.pathname === "/about"
-                      ? "nav-link active"
-                      : "nav-link"
-                  }
-                >
-                  About
-                </Link>
-              </li>
+          <li className="nav-item">
+            <Link
+              to="/about"
+              className={
+                window.location.pathname === "/about"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              About
+              </Link>
+          </li>
 
-              <li className="nav-item">
-                <Link
-                  to="/projects"
-                  className={
-                    window.location.pathname === "/projects"
-                      ? "nav-link active"
-                      : "nav-link"
-                  }
-                >
-                  Projects
-                </Link>
-              </li>
-              
-            </ul>
-          </div>
-        </nav>
-      );
+          <li className="nav-item">
+            <Link
+              to="/projects"
+              className={
+                window.location.pathname === "/projects"
+                  ? "nav-link active"
+                  : "nav-link"
+              }
+            >
+              Projects
+              </Link>
+          </li>
+
+        </ul>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar
