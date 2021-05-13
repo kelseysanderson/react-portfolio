920835659d12aafd72f396e98116146687c0fe78
kelseysanderson <78172284+kelseysanderson@users.noreply.github.com>
2021-05-13 00:21:05 -0500

    updates to app.js and app.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import Wrapper from "./components/Wrapper/Wrapper";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Projects from "./pages/Projects";
@@ -10,11 +11,13 @@ import './App.css';
 function App() {
   return (
     <Router>
-      <div className="main">
+      <div>
         <Navbar />
+        <Wrapper>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
           <Route exact path="/projects" component={Projects} />
+        </Wrapper>
         <Footer />
       </div>
     </Router>
