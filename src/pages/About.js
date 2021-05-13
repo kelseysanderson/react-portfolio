9dabdf2570e1e31fde8f95c450864cd37fb7deb8
kelseysanderson <78172284+kelseysanderson@users.noreply.github.com>
2021-05-13 00:20:20 -0500

    added images for projects page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,54 +2,58 @@ import React from "react";
 import "./about.css";
 import portraitone from "../images/portraitone.JPG"
 import portraittwo from "../images/portraittwo.jpg"
-import node from "../images/node.jpg"
-import javascript from "../images/js.jpg"
-import html from "../images/Html.jpg"
-import css from "../images/Css.jpg"
-import express from "../images/Express.jpg"
-import mysql from "../images/Mysql.jpg"
-import sequelize from "../images/Sequelize.jpg"
-import react from "../images/React.jpg"
-import mongo from "../images/mongodb.jpeg"
+import code from "../images/code.jpg"
+import mpls from "../images/mpls.jpg"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMapMarker } from '@fortawesome/free-solid-svg-icons'
+import { faLaptop } from '@fortawesome/free-solid-svg-icons'
+import { faUniversity } from '@fortawesome/free-solid-svg-icons'
+import { faEdit } from '@fortawesome/free-solid-svg-icons'
+import { faBriefcase } from '@fortawesome/free-solid-svg-icons'
+import { faHiking } from '@fortawesome/free-solid-svg-icons'
+import { faGamepad } from '@fortawesome/free-solid-svg-icons'
+
+
 
 function About() {
     return (
-        <div className="container">
-            <div className="row justify-content-around">
-                <div className="col-8 about-me-box">
-                    <h2>Bio</h2>
+        <div className="container-fluid">
+            <div className="row justify-content-around bio-row">
+                <div className="col-12 col-lg-6 mt-5 bio-box">
+                    <h2 id="bio-header">ABOUT ME</h2>
                     <ul id="bio-text">
-                        <li>Minneapolis, MN</li>
-                        <li>Therapist turned programmer</li>
-                        <li>Minneapolis, MN</li>
-                        <li>Therapist turned programmer</li>
-                        <li>Minneapolis, MN</li>
-                        <li>Therapist turned programmer</li>
-                        <li>Minneapolis, MN</li>
-                        <li>Therapist turned programmer</li>
-                        <li>Therapist turned programmer</li>
+                        <li className="bio-list-item"><FontAwesomeIcon icon={faMapMarker} /> Minneapolis, MN</li>
+                        <li className="bio-list-item"><FontAwesomeIcon icon={faLaptop} /> Therapist turned programmer</li>
+                        <li className="bio-list-item"><FontAwesomeIcon icon={faUniversity} /> Completed University of Minnesota Coding Bootcamp</li>
+                        <li className="bio-list-item"><FontAwesomeIcon icon={faEdit} /> Web Designer</li>
+                        <li className="bio-list-item"><FontAwesomeIcon icon={faBriefcase} /> Hard worker</li>
+                        <li className="bio-list-item"><FontAwesomeIcon icon={faHiking} /> Hiking/tennis/tv enthusiast</li>
+                        <li className="bio-list-item"><FontAwesomeIcon icon={faGamepad} /> Video game lover</li>
                     </ul>
                 </div>
-                <div className="col-3 about-me-box">
-                    <h2>Proficient Technologies</h2>
-                        <ul id="logos">
-                            <li class="logo" ><img src={html} alt="html logo" width="65" height="65" /></li> 
-                            <li class="logo" ><img src={css} alt="css logo" width="65" height="65" /></li>
-                            <li class="logo" ><img src={javascript} alt="jsavascript logo" width="65" height="65" /></li>
-                            <li class="logo" ><img src={react} alt="react logo" width="65" height="65" /></li>
-                            <li class="logo" ><img src={express} alt="express logo" width="65" height="65" /></li>  
-                            <li class="logo" ><img src={mysql} alt="mysql logo" width="65" height="65" /></li>
-                            <li class="logo" ><img src={mongo} alt="sequelize logo" width="75" height="50" />MongoDB</li>
-                            <li class="logo" ><img src={node} alt="node.js logo" width="65" height="65" /></li>
-                            <li class="logo" ><img src={sequelize} alt="sequelize logo" width="65" height="65" /> Sequelize</li>       
-                        </ul>
-                </div>
+                <div className="col-12 col-lg-5 mt-5 empty-box"></div>
             </div>
-            <div className="row">
-                <div className="col-6 col-md-3"><img src={portraitone} alt="potrait of Kelsey" className="pic"/></div>
-                <div className="col-6 col-md-3"><img src={portraitone} alt="potrait of Kelsey" className="pic"/></div>
-                <div className="col-6 col-md-3"><img src={portraittwo} alt="potrait of Kelsey" className="pic"/></div>
-                <div className="col-6 col-md-3"><img src={portraittwo} alt="potrait of Kelsey" className="pic"/></div>
+            <div className="row justify-content-around bio-row">
+                <div className="col-12 col-lg-6 mt-5">
+                    <div className="row picture-row justify-content-center">
+                        <div className="col-12 col-md-4 picture-column"><img src={portraittwo} alt="potrait of Kelsey" className="pic" /></div>
+                        <div className="col-12 col-md-4 picture-column"><img src={code} alt="potrait of Kelsey" className="pic" /></div>
+                        <div className="col-12 col-md-4 picture-column"><img src={mpls} alt="potrait of Kelsey" className="pic" /></div>
+                    </div>
+                </div>
+                <div className="col-12 col-lg-5 mt-5 tech-box">
+                    <h2 id="tech-header">PROFICIENT TECHNOLOGIES</h2>
+                    <ul id="logos">
+                        <li className="logo" >HTML</li>
+                        <li className="logo" >CSS </li>
+                        <li className="logo" >JavaScript</li>
+                        <li className="logo" >React</li>
+                        <li className="logo" >Express</li>
+                        <li className="logo" >MySQL</li>
+                        <li className="logo" >MongoDB</li>
+                        <li className="logo" >Node.js</li>
+                    </ul>
+                </div>
             </div>
         </div>
         /* // <section className="about-me-section">
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,32 +1,57 @@
 import React from "react";
+import modernMornings from "../images/modern-mornings.jpg"
+import techBlog from "../images/tech-blog.jpg"
+import userDirectory from "../images/user-directory-screencapture.jpg"
+import weatherDashboard from "../images/weather.jpg"
+import teamProfile from "../images/teamscreencapture.jpg"
+import fitnessTracker from "../images/stats-page.jpg"
+import {Carousel} from '3d-react-carousal';
+import "./project.css"
 
-function Project() {
-  return (
-      <section className="about-me-section">
-            <div className="container">
-                <div className="row d-flex justify-content-around mb-10 h-100" id="about-me-row">
-                    <div className="col-12 col-md-5 about-me-box align-self-center" id="resume-box">
-                        resume?
-                    </div>
-                    <div className="col-12 col-md-4 align-self-center">
-                        <div className="row">
-                            <div className="col-12 about-me-box" id="bio-box">
-                                bio
-                            </div>
-                        </div>
-                        <div className="row justify-content-around" id="picture-row">
-                            <div className="col-4 about-me-box" >
-                                pic1
-                            </div>
-                            <div className="col-4 about-me-box" >
-                                pic2
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-      </section>
-    );
+function Projects() {
+    let slides = [
+        <div>
+            <a href="https://kelseysanderson.github.io/user_directory/">
+            <img  src={userDirectory} alt="1" ></img>
+            <p className="project-description">React, HTML, CSS </p>
+            </a>
+        </div>,
+        <div>
+            <a href="https://radiant-plains-87467.herokuapp.com/">
+            <img  src={techBlog} alt="2" />
+            <p className="project-description">JavaScript, Node.js, Express, MySQL, Handlebars, CSS, Bootstrap   </p>
+            </a>
+        </div>,
+         <div>
+             <a href="https://kelseysanderson.github.io/weather_dashboard/">
+            <img  src={weatherDashboard} alt="2" />
+            <p className="project-description">JavaScript, jQuery, Jest, OpenWeather API, HTML, CSS, Bootstrap  </p>
+            </a>
+        </div>,
+        <div>
+            <a href="https://github.com/kelseysanderson/team_profile_generator">
+            <img  src={teamProfile} alt="2" />
+            <p className="project-description">JavaScript, Node.js, Jest, HTML, CSS, Bootstrap </p>
+            </a>
+        </div>,
+        <div>
+            <a href="https://slee612.github.io/group_project_1/">
+            <img  src={modernMornings} alt="2" />
+            <p className="project-description">JavaScript, jQuery, Reddit API, GNews API, HTML, CSS, Bootstrap </p>
+            </a>
+        </div>,
+        <div>
+            <a href="https://fitness-tracker-ksanderson.herokuapp.com/?id=608cdb8eca6bdc0015c60418">
+            <img  src={fitnessTracker} alt="2" />
+            <p className="project-description">JavaScript, Express, Node.js, MongoDB, HTML, CSS  </p>
+            </a>
+        </div>,   ];
+    
+    return(
+        <div className="container-fluid">
+            <Carousel slides={slides} autoplay={true} interval={6000}/>
+        </div>
+    )
 }
 
-export default Project;
+export default Projects;
